14f3a72e51e85c6a337f0cc72f133483a020ffe5
farhadahmed <farhadahmed2007@gmail.com>
2021-02-15 14:16:12 -0800

    create teachers in db from the UI

--- src/sample/NewTeacherDialogController.java
+++ src/sample/NewTeacherDialogController.java
@@ -1,7 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import sample.Model.Datasource;
+
 public class NewTeacherDialogController {
+    @FXML
+    private TextField teacherFirstNameTextField;
+    @FXML
+    private TextField teacherMiddleNameTextField;
+    @FXML
+    private TextField teacherLastNameTextField;
+    @FXML
+    private TextField teacherEmailTextField;
+    @FXML
+    private TextField teacherLoginIdTextField;
+    @FXML
+    private TextField teacherLoginPasswordTextField;
+
     public void createNewTeacher() {
-        // create teacher
+        // Create teacher
+        Datasource.datasource.createTeacher(teacherFirstNameTextField.getText(), teacherMiddleNameTextField.getText(), teacherLastNameTextField.getText(), teacherEmailTextField.getText(), teacherLoginIdTextField.getText(), teacherLoginPasswordTextField.getText());
     }
 }
